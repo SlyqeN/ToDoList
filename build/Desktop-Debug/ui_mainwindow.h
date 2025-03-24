9a1c7665b2ec5c8e7ca272f2576ea5e0cc28cd5e
SlyqeN <ivankarpov438999@gmail.com>
2025-03-24 18:53:59 +0300

    Add AddButton with logic

---
+++ build/Desktop-Debug/ui_mainwindow.h
@@ -0,0 +1,103 @@
+/********************************************************************************
+** Form generated from reading UI file 'mainwindow.ui'
+**
+** Created by: Qt User Interface Compiler version 6.8.2
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_MAINWINDOW_H
+#define UI_MAINWINDOW_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QLineEdit>
+#include <QtWidgets/QListWidget>
+#include <QtWidgets/QMainWindow>
+#include <QtWidgets/QMenuBar>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QStatusBar>
+#include <QtWidgets/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_MainWindow
+{
+public:
+    QWidget *centralwidget;
+    QPushButton *addButton;
+    QPushButton *deleteButton;
+    QListWidget *listWidget;
+    QLineEdit *lineEdit;
+    QPushButton *sortButton;
+    QMenuBar *menubar;
+    QStatusBar *statusbar;
+
+    void setupUi(QMainWindow *MainWindow)
+    {
+        if (MainWindow->objectName().isEmpty())
+            MainWindow->setObjectName("MainWindow");
+        MainWindow->resize(919, 600);
+        MainWindow->setStyleSheet(QString::fromUtf8("font: italic 11pt \"Caladea\";\n"
+"border-top-color: rgb(119, 118, 123);\n"
+"color: rgb(119, 118, 123);\n"
+""));
+        centralwidget = new QWidget(MainWindow);
+        centralwidget->setObjectName("centralwidget");
+        addButton = new QPushButton(centralwidget);
+        addButton->setObjectName("addButton");
+        addButton->setGeometry(QRect(10, 70, 201, 51));
+        addButton->setStyleSheet(QString::fromUtf8("font: italic 11pt \"Comfortaa\";\n"
+"color: rgb(51, 209, 122);"));
+        deleteButton = new QPushButton(centralwidget);
+        deleteButton->setObjectName("deleteButton");
+        deleteButton->setGeometry(QRect(330, 70, 221, 51));
+        deleteButton->setStyleSheet(QString::fromUtf8("font: italic 11pt \"Comfortaa\";\n"
+"color: rgb(237, 51, 59);"));
+        deleteButton->setIconSize(QSize(32, 32));
+        deleteButton->setCheckable(false);
+        listWidget = new QListWidget(centralwidget);
+        listWidget->setObjectName("listWidget");
+        listWidget->setGeometry(QRect(10, 130, 901, 291));
+        listWidget->setStyleSheet(QString::fromUtf8("color: rgb(222, 221, 218);"));
+        listWidget->setSortingEnabled(true);
+        lineEdit = new QLineEdit(centralwidget);
+        lineEdit->setObjectName("lineEdit");
+        lineEdit->setGeometry(QRect(10, 0, 901, 61));
+        sortButton = new QPushButton(centralwidget);
+        sortButton->setObjectName("sortButton");
+        sortButton->setGeometry(QRect(670, 70, 241, 51));
+        sortButton->setStyleSheet(QString::fromUtf8("font: italic 11pt \"Comfortaa\";\n"
+"alternate-background-color: rgb(192, 97, 203);\n"
+"border-top-color: rgb(51, 209, 122);"));
+        MainWindow->setCentralWidget(centralwidget);
+        menubar = new QMenuBar(MainWindow);
+        menubar->setObjectName("menubar");
+        menubar->setGeometry(QRect(0, 0, 919, 23));
+        MainWindow->setMenuBar(menubar);
+        statusbar = new QStatusBar(MainWindow);
+        statusbar->setObjectName("statusbar");
+        MainWindow->setStatusBar(statusbar);
+
+        retranslateUi(MainWindow);
+
+        QMetaObject::connectSlotsByName(MainWindow);
+    } // setupUi
+
+    void retranslateUi(QMainWindow *MainWindow)
+    {
+        MainWindow->setWindowTitle(QCoreApplication::translate("MainWindow", "MainWindow", nullptr));
+        addButton->setText(QCoreApplication::translate("MainWindow", "\320\224\320\276\320\261\320\260\320\262\320\270\321\202\321\214", nullptr));
+        deleteButton->setText(QCoreApplication::translate("MainWindow", "\320\243\320\264\320\260\320\273\320\270\321\202\321\214", nullptr));
+        sortButton->setText(QCoreApplication::translate("MainWindow", "C\320\276\321\200\321\202\320\270\321\200\320\276\320\262\320\260\321\202\321\214", nullptr));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class MainWindow: public Ui_MainWindow {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_MAINWINDOW_H
---
+++ mainwindow.cpp
@@ -0,0 +1,34 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QListWidgetItem>
+#include <QMessageBox>
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+void MainWindow::on_addButton_clicked()
+{
+    QString task = ui->lineEdit->text();
+    if (!task.isEmpty()) {
+        QListWidgetItem *item = new QListWidgetItem(task);
+        item->setFlags(item->flags() | Qt::ItemIsUserCheckable);
+        item->setCheckState(Qt::Unchecked);
+        ui->listWidget->addItem(item);
+        ui->lineEdit->clear();
+    }
+}
+
+// void MainWindow::on_deleteButton_clicked()
+// {
+//     qDeleteAll(ui->listWidget->selectedItems());
+// }
+
+// void MainWindow::on_sortButton_clicked()
+// {
+//     ui->listWidget->sortItems(sortOrder ? Qt::DescendingOrder : Qt::AscendingOrder);
+//     sortOrder = !sortOrder;
+// }
